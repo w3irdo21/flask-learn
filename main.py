a1282890af422fa19ecf2836a824485b969f9b39
Aastik <aastikgakhar01@gmail.com>
2023-01-27 12:44:18 +0530

    Learned about render_template,port change and much more

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
-@app.route('/')
+@app.route('/world')
 def hello_world() :
     return "Hello There, We learning Flask here"
 
+@app.route('/')
+def renderfunc():
+    return render_template('index.html')
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True, port=6115)
